0b4b1ef2280a8159deba3f7ddde74caebf122cd2
anastasiia <aserr184@gmail.com>
2025-02-20 16:57:28 +0300

    добавила комент к функциям

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -13,6 +13,7 @@ type Server struct {
 	pb.UnimplementedUserAPIV1Server
 }
 
+// Create создаёт новый объект на основе запроса
 func (s *Server) Create(_ context.Context, req *pb.CreateRequest) (*pb.CreateResponse, error) {
 
 	fmt.Printf("Create: %+v\n", req)
@@ -20,6 +21,7 @@ func (s *Server) Create(_ context.Context, req *pb.CreateRequest) (*pb.CreateRes
 
 }
 
+// Get получает объект по идентификатору
 func (s *Server) Get(_ context.Context, in *pb.GetRequest) (*pb.GetResponse, error) {
 	fmt.Printf("Get: %+v\n", in)
 	return &pb.GetResponse{
@@ -30,11 +32,13 @@ func (s *Server) Get(_ context.Context, in *pb.GetRequest) (*pb.GetResponse, err
 	}, nil
 }
 
+// Update обновляет объект на основе запроса.
 func (s *Server) Update(_ context.Context, in *pb.UpdateRequest) (*emptypb.Empty, error) {
 	fmt.Printf("Update: %+v\n", in)
 	return &emptypb.Empty{}, nil
 }
 
+// Delete удаляет объект по идентификатору.
 func (s *Server) Delete(_ context.Context, in *pb.DeleteRequest) (*emptypb.Empty, error) {
 	fmt.Printf("Delete: %+v\n", in)
 	return &emptypb.Empty{}, nil
